4c50956f2dc01a22f79b5523297e6ac3ebfc1c28
ROCO <dorumi714@gmail.com>
2020-10-13 22:41:10 +0900

    Update MovieCommentPattern.java

--- 002/Program1013/AnalysisAndProcess/MovieCommentPattern.java
+++ 002/Program1013/AnalysisAndProcess/MovieCommentPattern.java
@@ -11,21 +11,20 @@ public class MovieCommentPattern {
 				};
 		String[] tag = {"ABC", "WOW", "good", "Bad", "Papyrus", "DVE", "AABBCC", "AABXZA"};
 		int[] reply = new int[tag.length];
-		String regex = "[A-Za-z0-9<>~)/,!?('S.:]";
+		String regex = "[0-9<>~)/,!?('.:]";
 		
 		for (int i = 0; i < comment.length; i++) {
 			comment[i] = comment[i].replaceAll(regex,"");
-			System.out.println(comment[i]);
 			for (int j = 0; j < tag.length; j++) {
-				Pattern p = Pattern.compile(commnet[j]);
-				Matcher m = p.matcher(comment[j]);
-				while (m.find) {
-					reply++;
+				Pattern p = Pattern.compile(tag[j]);
+				Matcher m = p.matcher(comment[i]);
+				while (m.find()) {
+					reply[j]++;
 				}
 			}
-		for (int j= 0; j < reply.length; j++) System.out.println(tag[j]+" : "+reply[j]);
 		
 		}
+		for (int j= 0; j < tag.length; j++) System.out.println(tag[j]+" : "+reply[j]);
 	}
 }
 // reference : https://enterkey.tistory.com/353  https://m.blog.naver.com/PostView.nhn?blogId=bb_&logNo=220863282423&proxyReferer=https:%2F%2Fwww.google.com%2F

e6751171975ddb997bf045aedf7f1b1b8d59c3d0
Silica Durumi <Silica@Durumi.com>
2020-10-19 20:56:33 +0900

    1019 Exercise add comment

--- 003/1019/ExerciseEx02/Game369Ex.java
+++ 003/1019/ExerciseEx02/Game369Ex.java
@@ -6,16 +6,16 @@ import java.util.Vector;
 public class Game369Ex {
 
 	public static void Clap369Game() {
-		String userinputvalue;
-		int userinputchk, i;
-		int clapcount = 0;
-		String resultText =  "";
+		String userinputvalue; //사용자의 입력값
+		int userinputchk, i; // i 반복문 변수, userinputchk 사용자의 입력값이 범위에 맞는지 체크하는 변수
+		int clapcount = 0; // 칠 박수
+		String resultClapText =  ""; // 결과로 나오는 박수
 		Scanner scanner = new Scanner(System.in);
-		Vector<String> vector = new Vector<String>();
+		Vector<String> vector = new Vector<String>();//가변배열
 		
 		System.out.print("1~9999 사이의 값을 입력하세요>>");
 		userinputvalue = scanner.next();
-		userinputchk = Integer.parseInt(userinputvalue);
+		userinputchk = Integer.parseInt(userinputvalue);// 사용자의 입력값을 정수형으로 변환해서 user inputchk에 넣음
 		
 		if (userinputchk>=1&&userinputchk<=9999) {
 			for (i = 0; i < userinputvalue.length(); i++) 
@@ -25,9 +25,9 @@ public class Game369Ex {
 				if (vector.get(i).equals("3")||vector.get(i).equals("6")||vector.get(i).equals("9")) 
 					clapcount++;
 				if(clapcount>0){
-					resultText = "박수";
+					resultClapText = "박수";
 				for ( i = 0; i < clapcount; i++) 
-					resultText +=  "짝";
+					resultClapText +=  "짝";
 				}
 				else
 					System.out.println("입력된 값에 3,6,9가 없습니다.  ");
@@ -35,6 +35,6 @@ public class Game369Ex {
 		else
 			System.out.println("입력된 값이 범위를 초과 하였습니다.");
 		
-		System.out.println(resultText);
+		System.out.println(resultClapText);
 	}
 }
--- 003/1019/ExerciseEx2MainMenu.java
+++ 003/1019/ExerciseEx2MainMenu.java
@@ -2,7 +2,7 @@ import ExerciseEx02.*;
 
 public class ExerciseEx2MainMenu {
     public static void main(String[] args) {
- //       ID_NumberEx002.ReturnBirthDay();
+        ID_NumberEx002.ReturnBirthDay();
         Game369Ex.Clap369Game();
     }
 }
